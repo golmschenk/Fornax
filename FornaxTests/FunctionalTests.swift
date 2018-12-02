26f326fe1c286112bc17ea62ccfa1cd4b3bd4dc4
Greg Olmschenk <golmschenk@gmail.com>
2018-12-02 00:06:07 -0500

    Add gitignore

---
+++ FornaxTests/FunctionalTests.swift
@@ -0,0 +1,26 @@
+//
+//  FunctionalTests.swift
+//  FornaxTests
+//
+//  Created by Greg Olmschenk on 12/1/18.
+//  Copyright © 2018 Greg Olmschenk. All rights reserved.
+//
+
+import XCTest
+
+class FunctionalTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
